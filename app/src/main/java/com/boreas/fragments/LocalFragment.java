4442ff4d4e02bd3c6c8064f49dc4d172ffc778c7
Kalyan <kalyanponduri@gmail.com>
2020-03-15 12:15:32 -0700

    Project UI
    Porject UI

---
+++ app/src/main/java/com/boreas/fragments/LocalFragment.java
@@ -0,0 +1,65 @@
+package com.boreas.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.boreas.activities.ChatActivity;
+import com.boreas.adapters.ChatsAdapter;
+import com.e.boreas.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LocalFragment extends Fragment {
+
+    public static final String EXTRA_TAB_NAME = "tab_name";
+    private ChatActivity mParent;
+    private View rootView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mParent = (ChatActivity) getActivity();
+    }
+
+    public static LocalFragment newInstance(String tabName) {
+        Bundle args = new Bundle();
+        args.putString(EXTRA_TAB_NAME, tabName);
+        LocalFragment fragment = new LocalFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        rootView = inflater.inflate(R.layout.fragment_local, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initUI();
+    }
+
+    private void initUI() {
+
+    }
+
+}
